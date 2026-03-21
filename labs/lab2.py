8f62eb8f23026d042861d192dc82657d6a677405
Harsh Raj <harshraj.contact@gmail.com>
2026-03-21 22:06:25 +0530

    lab 2

---
+++ labs/lab2.py
@@ -0,0 +1,26 @@
+def apply_discount(price, discount):
+
+    if not isinstance(price, (int, float)):
+        return "The price should be a number."
+
+    if not isinstance(discount, (int, float)):
+        return "The discount should be a number."
+
+    if price <= 0:
+        return "The price should be greater than 0."
+    
+    if discount < 0 or discount > 100:
+        return "The discount should be between 0 and 100."
+
+    else:
+        cal_dis = (discount*price)/100
+
+    final_price = price - cal_dis
+
+    return print(final_price)
+
+apply_discount(100, 20)
+apply_discount(200, 50)
+apply_discount(50, 0)
+apply_discount(50, 100)
+apply_discount(74.5, 20.0)
