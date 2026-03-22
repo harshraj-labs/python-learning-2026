4c5172b9fd7fc4f6fb259c33d22337d77997b7d4
Harsh Raj <harshraj.contact@gmail.com>
2026-03-23 00:37:26 +0530

    exciting lab man

---
+++ labs/lab3.py
@@ -0,0 +1,28 @@
+#Build an RPG Character
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(ch_name, strength, intelligence,charisma):
+    if not isinstance(ch_name,str):
+        return ("The character name should be a string")
+    if ch_name == '':
+        return ("The character should have a name")
+    if len(ch_name)> 10:
+        return ("The character name is too long")
+    if ' ' in ch_name:
+        return ("The character name should not contain spaces")
+
+    if not isinstance(strength,int) or not isinstance(intelligence,int) or not isinstance(charisma,int):
+        return ("All stats should be integers")
+    if strength<1 or intelligence<1 or charisma<1:
+        return ("All stats should be no less than 1")
+    if strength>4 or intelligence>4 or charisma>4:
+        return ("All stats should be no more than 4") 
+    if strength + intelligence + charisma != 7 :
+        return ("The character should start with 7 points")
+    
+    else:
+        return (f"{ch_name}\nSTR {strength*full_dot}{(10-strength)*empty_dot}\nINT {intelligence*full_dot}{(10-intelligence)*empty_dot}\nCHA {charisma*full_dot}{(10-charisma)*empty_dot}")
+
+
+print(create_character("ren",4,2,1))
