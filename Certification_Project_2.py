b71e21c235eb9cf2d2d39abc35c3659624d95387
Harsh Raj <harshraj.contact@gmail.com>
2026-06-01 22:00:33 +0530

    basic setup

--- Certification_Project_2.py
+++ Certification_Project_2.py
@@ -1,1 +1,18 @@
 # Building a Budget App
+
+class Category:
+    
+    def __init__(self,name):
+        self.name = name
+        ledger = []
+
+    def deposit(self,amount,description):
+        self.amount = amount
+        self.description = description
+        if not description:
+            description =''
+        
+        
+
+def create_spend_chart(categories):
+    pass
