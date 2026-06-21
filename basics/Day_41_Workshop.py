6f626fcc10c9854d99c382c3f31ce4ed7c3fa22a
Harsh Raj <harshraj.contact@gmail.com>
2026-06-21 22:03:09 +0530

    discount workshop

---
+++ basics/Day_41_Workshop.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class Product:
+    def __init__(self, name: str, price: float) -> None:
+        self.name = name
+        self.price = price
+
+    def __str__(self) -> str:
+        return f'{self.name} - ${self.price}'
+
+class DiscountStrategy(ABC):
+    @abstractmethod
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        pass
+
+    @abstractmethod
+    def apply_discount(self, product: Product) -> float:
+        pass
+
+class PercentageDiscount(DiscountStrategy):
+    def __init__(self, percent: int) -> None:
+        self.percent = percent
+
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        return self.percent <= 70
+
+    def apply_discount(self, product: Product) -> float:
+        return product.price * (1 - self.percent / 100)
+class FixedAmountDiscount(DiscountStrategy):
+    def __init__(self,amount: int) ->  None:
+        self.amount = amount
+product = Product('Wireless Mouse', 50.0)
+print(product)
+
+discount = PercentageDiscount(10)
+print(discount.apply_discount(product))
