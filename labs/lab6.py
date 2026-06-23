8779f8642fddd647dbc457e280a1125aaa7ee69e
Harsh Raj <harshraj.contact@gmail.com>
2026-06-23 22:38:20 +0530

    completed lab

--- labs/lab6.py
+++ labs/lab6.py
@@ -1,1 +1,46 @@
 #Build a Player Interface
+from abc import ABC, abstractmethod
+import random
+
+
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0, 0)
+        self.path = [self.position]
+
+    def make_move(self):
+        random_move = random.choice(self.moves)
+
+        self.position = (
+            self.position[0] + random_move[0],
+            self.position[1] + random_move[1]
+        )
+
+        self.path.append(self.position)
+
+        return self.position
+
+    @abstractmethod
+    def level_up(self):
+        pass
+
+
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+
+        self.moves = [
+            (0, 1),
+            (0, -1),
+            (-1, 0),
+            (1, 0)
+        ]
+
+    def level_up(self):
+        self.moves.extend([
+            (-1, 1),
+            (1, 1),
+            (-1, -1),
+            (1, -1)
+        ])
