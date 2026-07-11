01cf4ea7d0d5e8b6eeeabe9ab1d54c53ef865bf2
Harsh Raj <harshraj.contact@gmail.com>
2026-07-12 02:08:21 +0530

    Implement basic HashTable class with CRUD methods

--- Certification_Project_4.py
+++ Certification_Project_4.py
@@ -1,1 +1,27 @@
-# Building a Hash table
+class HashTable:
+    def __init__(self):
+        self.collection = {}
+
+    def hash(self,word):
+        return sum(ord(character) for character in word)
+    
+    def add(self,key,value):
+        hash_key = self.hash(key)
+
+        if hash_key not in self.collection:
+            self.collection[hash_key] = {}
+        
+        self.collection[hash_key][key] = value
+    
+    def remove(self,key):
+        hash_key = self.hash(key)
+
+        if hash_key in self.collection and key in self.collection[hash_key]:
+            del self.collection[hash_key][key]
+            if not self.collection[hash_key]:
+                del self.collection[hash_key]
+    
+    def lookup(self,key):
+        hash_key = self.hash(key)
+        if hash_key in self.collection and key in self.collection[hash_key]:
+            return self.collection[hash_key][key]
