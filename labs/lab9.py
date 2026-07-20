7da7eac5d413db55c410c33e3e175836d39726f6
Harsh Raj <harshraj.contact@gmail.com>
2026-07-20 21:58:14 +0530

    selection sort

--- labs/lab9.py
+++ labs/lab9.py
@@ -1,1 +1,12 @@
-
+# Selection Sort
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i+1,len(arr)):
+            if arr[j]<arr[min_index]:
+                min_index = j
+        
+        if min_index != i:
+            arr[i],arr[min_index] = arr[min_index],arr[i]
+        
+    return arr
