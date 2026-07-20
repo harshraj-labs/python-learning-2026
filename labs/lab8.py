09b5f97e7a2b218ff0a92b71b2cd2de9ce236bf8
Harsh Raj <harshraj.contact@gmail.com>
2026-07-20 21:29:15 +0530

    quick sort algorithm

--- labs/lab8.py
+++ labs/lab8.py
@@ -1,1 +1,18 @@
 # Quick Sort Implementation
+def quick_sort(arr):
+    if len(arr)<=1:
+        return arr
+    
+    pivot = arr[0]
+    less =[]
+    equal =[]
+    greater =[]
+    for num in arr:
+        if num<pivot:
+            less.append(num)
+        elif num == pivot:
+            equal.append(num)
+        else:
+            greater.append(num)
+        
+    return quick_sort(less)+equal+quick_sort(greater)
